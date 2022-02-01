0b3aa8dd92a4f863a852a46f66e608697e77c50f
odley13 <franciscofrancieldo83@gmail.com>
2022-01-31 22:23:33 -0300

    Commit inserido todas as dependencies, e plugins.

---
+++ app/src/main/java/com/example/businesscard_cartao_de_visitas/data/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.businesscard_cartao_de_visitas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
