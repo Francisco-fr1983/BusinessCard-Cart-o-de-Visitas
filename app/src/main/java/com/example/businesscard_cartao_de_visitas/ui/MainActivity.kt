7253625070d2dd7a4fc2d2214bb2bae1fba8bf7b
odley13 <franciscofrancieldo83@gmail.com>
2022-01-31 23:29:06 -0300

    Commit Na configuração do binding, foi verificado que pasta generated não foi criado as pastas do binding, realizar clean Projeto e Rebuild, para que apareça as pastas e Binding posso funcionar.

--- app/src/main/java/com/example/businesscard_cartao_de_visitas/ui/MainActivity.kt
+++ app/src/main/java/com/example/businesscard_cartao_de_visitas/ui/MainActivity.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 import com.example.businesscard_cartao_de_visitas.R
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {ActivityMainBinding}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
