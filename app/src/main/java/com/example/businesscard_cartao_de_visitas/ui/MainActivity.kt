408567b7dc36575ca09f3483538414ec5f3fadd6
odley13 <franciscofrancieldo83@gmail.com>
2022-02-01 21:31:58 -0300

    Commit Resolvido o problema do ViewBinding, feito novamente a sincronização da gradle com a configuração correta.

--- app/src/main/java/com/example/businesscard_cartao_de_visitas/ui/MainActivity.kt
+++ app/src/main/java/com/example/businesscard_cartao_de_visitas/ui/MainActivity.kt
@@ -1,15 +1,15 @@
 package com.example.businesscard_cartao_de_visitas.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.businesscard_cartao_de_visitas.R
+import androidx.appcompat.app.AppCompatActivity
+import com.example.businesscard_cartao_de_visitas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private val binding by lazy {ActivityMainBinding}
+    private val binding by lazy {ActivityMainBinding.inflate(layoutInflater)}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
