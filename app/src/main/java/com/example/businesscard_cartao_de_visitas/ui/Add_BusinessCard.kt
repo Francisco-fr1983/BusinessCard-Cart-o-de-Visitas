739d82b88583d12bfa659d888a50266b82561c23
odley13 <franciscofrancieldo83@gmail.com>
2022-02-05 20:37:22 -0300

    Commit Create activity Add_BusinessCard.kt, and layout activity_add__business_card.xml.

---
+++ app/src/main/java/com/example/businesscard_cartao_de_visitas/ui/Add_BusinessCard.kt
@@ -0,0 +1,11 @@
+package com.example.businesscard_cartao_de_visitas.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Add_BusinessCard : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add__business_card)
+    }
+}
