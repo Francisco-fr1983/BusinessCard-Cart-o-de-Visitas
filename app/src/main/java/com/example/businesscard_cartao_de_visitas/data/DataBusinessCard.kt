ea47cdb049d195ce440ae247ec8d1d24eae8640a
odley13 <franciscofrancieldo83@gmail.com>
2022-02-06 18:42:21 -0300

    Commit create the data class DataBusinessCard, para passar quais informações serão gravadas no banco de dados.

---
+++ app/src/main/java/com/example/businesscard_cartao_de_visitas/data/DataBusinessCard.kt
@@ -0,0 +1,22 @@
+package com.example.businesscard_cartao_de_visitas.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+//Anotação que faz a integração com o Room
+@Entity
+data class DataBusinessCard (
+
+    //O data class vai passar as informações que o banco de dados precisa gravar.
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val nome: String,
+    val empresa: String,
+    val telefone: String,
+    val email: String,
+    val corPersonalizada: String
+
+
+
+
+)
--- app/src/main/java/com/example/businesscard_cartao_de_visitas/ui/AddBusinessCard.kt
+++ app/src/main/java/com/example/businesscard_cartao_de_visitas/ui/AddBusinessCard.kt
@@ -22,4 +22,5 @@ class AddBusinessCard : AppCompatActivity() {
         }
 
     }
+
 }
