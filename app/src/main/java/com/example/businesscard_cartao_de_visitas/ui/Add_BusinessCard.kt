3aef72749ffcc83c8635a8caf0fb8bb7a9519505
odley13 <franciscofrancieldo83@gmail.com>
2022-02-05 21:16:09 -0300

    Commit Especial android:background="@drawable/quebra_cabeça_autismo_oficial"

--- app/src/main/java/com/example/businesscard_cartao_de_visitas/ui/Add_BusinessCard.kt
+++ app/src/main/java/com/example/businesscard_cartao_de_visitas/ui/Add_BusinessCard.kt
@@ -2,10 +2,13 @@ package com.example.businesscard_cartao_de_visitas.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.businesscard_cartao_de_visitas.databinding.ActivityMainBinding
 
 class Add_BusinessCard : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater)}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add__business_card)
+        setContentView(binding.root)
     }
 }
