bbdbdae57904a3346b1fcc58bab6b1282df017b7
odley13 <franciscofrancieldo83@gmail.com>
2022-02-08 10:35:46 -0300

    Commit create inner class ViewHolder

--- app/src/main/java/com/example/businesscard_cartao_de_visitas/ui/AdapterBuninessCard.kt
+++ app/src/main/java/com/example/businesscard_cartao_de_visitas/ui/AdapterBuninessCard.kt
@@ -1,10 +1,24 @@
 package com.example.businesscard_cartao_de_visitas.ui
 
+import android.view.View
 import android.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
 import com.example.businesscard_cartao_de_visitas.data.DataBusinessCard
+import com.example.businesscard_cartao_de_visitas.databinding.ItensBusinesscardBinding
+
 
 
 //Criar o adapter para pegar os dados que estão vindo da lista, e jogar na tela para o usuário ver, ele é responsável pela exibição dos dados de lista na tela.
 class AdapterBuninessCard: ListAdapter<DataBusinessCard, AdapterBuninessCard.ViewHolder>(DiffCallback()) {
+
+    var listenerShare: (View) -> Unit = {}
+
+    inner class ViewHolder(
+            private val binding: ItensBusinesscardBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind (item:DataBusinessCard) {
+
+        }
+    }
 
 }
