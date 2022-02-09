c483421d27c38ad4750a95a398e34c307394af34
odley13 <franciscofrancieldo83@gmail.com>
2022-02-09 19:27:07 -0300

    Commit Após finalização do adapter, temos que chamar ele lá na MainActivity.

--- app/src/main/java/com/example/businesscard_cartao_de_visitas/ui/AdapterBuninessCard.kt
+++ app/src/main/java/com/example/businesscard_cartao_de_visitas/ui/AdapterBuninessCard.kt
@@ -1,11 +1,14 @@
 package com.example.businesscard_cartao_de_visitas.ui
 
+import android.graphics.Color
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.ListAdapter
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.businesscard_cartao_de_visitas.data.DataBusinessCard
 import com.example.businesscard_cartao_de_visitas.databinding.ItensBusinesscardBinding
-
 
 
 //Criar o adapter para pegar os dados que estão vindo da lista, e jogar na tela para o usuário ver, ele é responsável pela exibição dos dados de lista na tela.
@@ -13,12 +16,40 @@ class AdapterBuninessCard: ListAdapter<DataBusinessCard, AdapterBuninessCard.Vie
 
     var listenerShare: (View) -> Unit = {}
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItensBusinesscardBinding.inflate(inflater, parent, false)
+
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
     inner class ViewHolder(
             private val binding: ItensBusinesscardBinding
     ) : RecyclerView.ViewHolder(binding.root) {
         fun bind (item:DataBusinessCard) {
+            binding.tvName.text = item.nome
+            binding.tvEmpresa.text = item.empresa
+            binding.tvPhone.text = item.telefone
+            binding.tvEmail.text = item.email
+            binding.mcvContent.setCardBackgroundColor(Color.parseColor(item.corPersonalizada))
+            binding.mcvContent.setOnClickListener{
+                listenerShare(it)
+            }
 
         }
     }
 
 }
+
+class DiffCallback: DiffUtil.ItemCallback<DataBusinessCard>() {
+    override fun areItemsTheSame(oldItem: DataBusinessCard, newItem: DataBusinessCard) = oldItem == newItem
+
+    override fun areContentsTheSame(oldItem: DataBusinessCard, newItem: DataBusinessCard) = oldItem.id == newItem.id
+
+}
+
+//Após finalização do adapter, temos que chamar ele lá na MainActivity.
