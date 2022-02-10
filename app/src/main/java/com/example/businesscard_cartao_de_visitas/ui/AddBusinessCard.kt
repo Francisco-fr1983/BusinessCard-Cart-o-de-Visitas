d2a34d2c2794a156bf602274a30bd1b01c369dbf
odley13 <franciscofrancieldo83@gmail.com>
2022-02-10 10:33:42 -0300

    Commit  Com a criação do ViewModel, no AddBusinessCard.kt, e com a instancia do mainViewModel.insert(dataBusinessCard), desse jeito haverá a persistencia dos dados.

--- app/src/main/java/com/example/businesscard_cartao_de_visitas/ui/AddBusinessCard.kt
+++ app/src/main/java/com/example/businesscard_cartao_de_visitas/ui/AddBusinessCard.kt
@@ -1,12 +1,20 @@
 package com.example.businesscard_cartao_de_visitas.ui
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.example.businesscard_cartao_de_visitas.App
 import com.example.businesscard_cartao_de_visitas.databinding.ActivityAddBusinessCardBinding
+import com.example.businesscard_cartao_de_visitas.data.DataBusinessCard
 
 class AddBusinessCard : AppCompatActivity() {
 
     private val binding by lazy { ActivityAddBusinessCardBinding.inflate(layoutInflater)}
+
+    private val mainViewModel: MainViewModel by viewModels {
+        MainViewModelFactory((application as App).repository)
+        //Pois estamos injentando o repository dentro do ViewModel, para que faça o controle.
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +26,19 @@ class AddBusinessCard : AppCompatActivity() {
     private fun insertListener() {
         binding.btnClose.setOnClickListener {
             finish()
+
+        }
+        binding.btnGravar.setOnClickListener {
+            val dataBusinessCard = DataBusinessCard(
+                nome = binding.tilName.editText?.text.toString(),
+                empresa = binding.tilEmpresa.editText?.text.toString(),
+                telefone = binding.tilPhone.editText?.text.toString(),
+                email = binding.tilEmail.editText?.text.toString(),
+                corPersonalizada = binding.tilCorDeFundo.editText?.text.toString()
+
+            )
+            mainViewModel.insert(dataBusinessCard)
+            //Com a criação do ViewModel, no AddBusinessCard.kt, e com a instancia do mainViewModel.insert(dataBusinessCard), desse jeito haverá a persistencia dos dados.
 
         }
 
